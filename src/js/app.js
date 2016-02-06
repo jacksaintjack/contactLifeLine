b5c1b1f82ec6cf344e88417a6e45b5b32de2492e
Nicholas Glover <jacksaintjack@gmail.com>
2016-02-06 12:31:08 -0600

    Added all the components we need for the project

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
 import { hashHistory } from 'react-router'
-//Remove this later
+
 import NavBar from './components/navBar.js';
 
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path='/' component={NavBar}>
-
+        <IndexRoute component={Home}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
