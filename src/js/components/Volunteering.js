5b716199c9a3d5f52d32204bed7ee8bf9b5dc5a1
Nicholas Glover <jacksaintjack@gmail.com>
2016-04-26 21:50:41 -0500

    Started work on the other pages

--- src/js/components/Volunteering.js
+++ src/js/components/Volunteering.js
@@ -3,13 +3,62 @@ import React, { PropTypes } from 'react'
 class Volunteering extends React.Component {
   render () {
     return (
-    <section className="volunteerContainer">
-      <h1>Four Hour Reassurance Training or Twenty-four Hour Crisis Training!</h1>
-      <p>Reassurance Training-  Have you ever spent time with someone who was 100 years young talking of the things she/he had done? Have you ever laughed with someone who was 93 and was reading " Fifty Shades of Grey"? Learn how to be the link of life for those who are home-bound or Elderly. Share 5-10 minutes a day talking, laughing, and sharing life's wisdom with those who have experienced life in a way that will never exist again.  Be that person that assures someone who is home-bound due to depression or a disability that life is worth holding on to.</p>
-      <p>Crisis Line Training- Yes it is in-depth training. You learn about Active Listening; What makes a crisis; Mental Illness; and Suicide Prevention/ Intervention. These are all critical Life Saving Topics; however, the joy you get from just being there for someone in their time of need is worth it. NO NOT ALL CALLS ARE SUICIDE RELATED. You might get one a month. You might get more depending on the season, but you get more calls from those who just need someone to HEAR what they are saying. Imagine a time when you just needed to scream because no one was getting what you were saying. That is what 98% of our Crisis calls are about, someone who just needs to be heard.</p>
-      <p>Support Worker Training- You go through the same training as a Crisis Worker; however, you have to be a Professional who can help take those critical calls when the Volunteer is uncomfortable. This is for Counselors, Therapists, Clergy, Medical Staff. Support Staff only work once or twice a month, unless you are comfortable with more. We understand your schedules are full, but that you have a Compassion to help others.</p>
-      <p>If you don't want to work the phones, how about Office Support. Monitor Facebook, Tweeter, and Instagram. Are you someone who likes to build and update websites? Maybe you like to work special events? Fundraising? Are you a budding journalist? We are building a News Letter.  There are many ways to Volunteer with Contact. You choose what level you are comfortable with.</p>
-    </section>
+      <section className="volunteerContainer">
+        <header>
+          <h3>
+            Give, and it will be given to you. A good measure, pressed down,
+            shaken together and running over, will be poured into your lap.
+            For with the measure you use, it will be measured to you.”
+          </h3>
+          <h4>Luke 6:38</h4>
+        </header>
+        <section>
+          <h1>Four Hour Reassurance Training or Twenty-four Hour Crisis Training!</h1>
+          <p>
+            Reassurance Training-  Have you ever spent time with someone who was 100
+            years young talking of the things she/he had done?
+            Have you ever laughed with someone who was 93 and was reading
+            " Fifty Shades of Grey"? Learn how to be the link of life for those who
+            are home-bound or Elderly. Share 5-10 minutes a day talking, laughing,
+            and sharing life's wisdom with those who have experienced life in a way
+            that will never exist again.  Be that person that assures someone who is
+            home-bound due to depression or a disability that life is worth holding on
+            to.
+          </p>
+
+          <p>
+            Crisis Line Training- Yes it is in-depth training.
+            You learn about Active Listening; What makes a crisis;
+            Mental Illness; and Suicide Prevention/ Intervention.
+            These are all critical Life Saving Topics; however,
+            the joy you get from just being there for someone in their time of need
+            is worth it. NO NOT ALL CALLS ARE SUICIDE RELATED.
+            You might get one a month. You might get more depending on the season,
+            but you get more calls from those who just need someone to HEAR what
+            they are saying. Imagine a time when you just needed to scream because
+            no one was getting what you were saying. That is what 98% of our Crisis
+            calls are about, someone who just needs to be heard.
+          </p>
+          <p>
+            Support Worker Training- You go through the same training as a
+            Crisis Worker; however, you have to be a Professional who can help
+            take those critical calls when the Volunteer is uncomfortable.
+            This is for Counselors, Therapists, Clergy, Medical Staff.
+            Support Staff only work once or twice a month, unless you are
+            comfortable with more. We understand your schedules are full,
+            but that you have a Compassion to help others.
+          </p>
+
+          <p>
+            If you don't want to work the phones, how about Office Support.
+            Monitor Facebook, Twitter, and Instagram. Are you someone who likes to build and update websites?
+            Maybe you like to work special events? Fundraising?
+            Are you a budding journalist? We are building a News Letter.
+            There are many ways to Volunteer with Contact.
+            You choose what level you are comfortable with.
+          </p>
+        </section>
+      </section>
     )
   }
 }
