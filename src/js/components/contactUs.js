379cf473955b4746c892d6db116fec06fdc80edc
Nicholas Glover <jacksaintjack@gmail.com>
2016-05-07 17:17:03 -0500

    I have finished up the desktop look of the website

--- src/js/components/contactUs.js
+++ src/js/components/contactUs.js
@@ -2,7 +2,29 @@ import React, { PropTypes } from 'react'
 
 class ContactUs extends React.Component {
   render () {
+    return (
+      <section className='contactusContainer'>
+        <section className='contactusText'>
+          <h1>Contact Information</h1>
 
+          <h2>Helpline Number</h2>
+          <h3><a href='tel://1.800.454.8336'>1-800-454-8336</a></h3>
+
+          <h2>Business Line</h2>
+          <h3><a href='tel://931.246.0754'>(931) 247-0754</a></h3>
+
+          <h2>Email</h2>
+          <h3><a href='mailto:support@example.com'>support@example.com</a></h3>
+
+          <h2>FIND US ON:</h2>
+
+          <h3><a className='facebookLink' href='https://www.facebook.com/Contact-Lifeline-of-the-Highland-Rim-195788343778294/'>FACEBOOK</a></h3>
+          <h3><a className='twiiterLink' href='https://twitter.com/contact_hope'>INSTAGRAM</a></h3>
+          <h3><a className='instagramLink' href='https://www.instagram.com/contactlifelinehr/'>TWITTER</a></h3>
+
+        </section>
+      </section>
+    )
   }
 }
 
--- src/js/components/ourHistory.js
+++ src/js/components/ourHistory.js
@@ -4,11 +4,48 @@ class OurHistory extends React.Component {
   render () {
     return(
       <section className="historyContainer">
-        <p>Contact Lifeline of the Highland Rim, Inc. is a twenty-four hour help line, offered free of charge, to all residents of Bedford, Coffee, Franklin, Grundy, Lincoln, Moore and Warren counties in Middle Tennessee. Grounded in the Judea-Christian tradition, Contact offers crisis intervention, resource information and reassurance. The organization recruits, equips, and supports volunteers as they respond to callers with non-judgmental, non-directive, active, and reflective listening.</p>
-        <p>The Crisis Center was opened in March of 1981 by a small Christian group of elderly volunteers, led by Janet Fennimore, who had the desire to not sit back and take what life was dishing out. The concept of Contact comes out of Australia, by a group of Christians who felt that Jesus’ ministry was sorely missing in this world today. Jesus walked this earth helping those in need, especially the lost and confused, the stricken and the lame, and the forgotten souls left to fend for them selves. Contact grew and it flourished over the years here in the Highland Rim area for 29 years, with many boards taking over their dream as time went by. </p>
-        <p>In 2010, Contact Lifeline was facing the dilemma of shutting its doors forever. Who would tend to those who cry out in the night? Who would be there for that small child scared and hovering in a corner as mommy and daddy fight? Who would be there for that teen that is confused and lost, with no one to talk to? Who would be there for that addict who is reaching out in the middle of the night to stop the insanity they call life? Most importantly, who would be there for the home bound, the known suicidal victim, the elder who lives alone to make sure that they woke up today, to make sure they took their medications, to make sure they ate something, to make sure they heard a friendly voice today? Who would be there for the caller who just needs an ear that listens? Who would be there?</p>
-        <p>In November of 2010, Family Counseling Center of Middle Tennessee and it's board of Directors said," WE WILL DO IT". We will take on the fight started in 1981. We will do what it takes to assure the calls are answered and people are reached.On December 24, 2010 Contact came up under the helm of the Family Counseling Center, led by David Pate, and it's Board of Directors, led by Ron Fuller. We are working hard to rekindle the hearts of people everywhere. We need to ban together as a community and fight to keep the hope alive. If you have an empty hour or 10 and need a constructive way to fill it—call us. If you are a retired person who is just not ready to sit down and stop living—call us. If you are a mother, a teen, or just someone who cares—CALL US! If you are someone who wants to help others but doesn’t know how---CALL US! We need you. We will work with YOUR schedule, we will train you, and we will walk you through it. The only thing it will cost you--- your time and service with a lifetime of fulfillment.</p>
-        <p>More than 43 years ago, the dream became a reality not only in Australia, but around the world, including the United States. Although every Contact Center is unique in the way it operates, Contact boards and staffs around the world share common belief system:</p>
+        <section className='historyText'>
+        <h1>History</h1>
+
+        <p>
+          Contact Lifeline of the Highland Rim, Inc. is a twenty-four hour help line,
+          offered free of charge, to all residents of Bedford, Coffee, Franklin, Grundy, Lincoln, Moore and Warren counties in Middle Tennessee.
+          Grounded in the Judea-Christian tradition, Contact offers crisis intervention, resource information and reassurance.
+          The organization recruits, equips, and supports volunteers as they respond to callers with non-judgmental, non-directive, active, and reflective listening.
+        </p>
+
+        <p>
+          The Crisis Center was opened in March of 1981 by a small Christian group of elderly volunteers, led by Janet Fennimore, who had the desire to not sit back and take what life was dishing out.
+          The concept of Contact comes out of Australia, by a group of Christians who felt that Jesus’ ministry was sorely missing in this world today.
+          Jesus walked this earth helping those in need, especially the lost and confused, the stricken and the lame, and the forgotten souls left to fend for them selves.
+          Contact grew and it flourished over the years here in the Highland Rim area for 29 years,
+          with many boards taking over their dream as time went by.
+        </p>
+
+        <p>
+          In 2010, Contact Lifeline was facing the dilemma of shutting its doors forever. Who would tend to those who cry out in the night?
+          Who would be there for that small child scared and hovering in a corner as mommy and daddy fight? Who would be there for that teen that is confused and lost,
+          with no one to talk to? Who would be there for that addict who is reaching out in the middle of the night to stop the insanity they call life?
+          Most importantly, who would be there for the home bound, the known suicidal victim, the elder who lives alone to make sure that they woke up today,
+          to make sure they took their medications, to make sure they ate something, to make sure they heard a friendly voice today?
+          Who would be there for the caller who just needs an ear that listens? Who would be there?
+        </p>
+
+        <p>
+          In November of 2010, Family Counseling Center of Middle Tennessee and it's board of Directors said," WE WILL DO IT". We will take on the fight started in 1981.
+          We will do what it takes to assure the calls are answered and people are reached.On December 24, 2010 Contact came up under the helm of the Family Counseling Center,
+          led by David Pate, and it's Board of Directors, led by Ron Fuller. We are working hard to rekindle the hearts of people everywhere.
+          We need to ban together as a community and fight to keep the hope alive. If you have an empty hour or 10 and need a constructive way to fill it—call us.
+          If you are a retired person who is just not ready to sit down and stop living—call us. If you are a mother, a teen, or just someone who cares—CALL US!
+          If you are someone who wants to help others but doesn’t know how---CALL US! We need you. We will work with YOUR schedule, we will train you, and we will walk you through it.
+          The only thing it will cost you--- your time and service with a lifetime of fulfillment.
+        </p>
+
+        <p>
+          More than 43 years ago, the dream became a reality not only in Australia, but around the world, including the United States. Although every Contact Center is unique in the way it operates,
+          Contact boards and staffs around the world share common belief system:
+        </p>
+
         <ol>
           <li>A belief in the importance of providing round-the-clock caring and listening for the community.</li>
           <li>A belief in the importance of creating a positive image among other agencies so that Contact is respected and admired in the community</li>
@@ -16,11 +53,17 @@ class OurHistory extends React.Component {
           <li>The belief that everyone who joins Contact is regarded as equal, requiring the same training and the same rules. We all begins from the same base and grow from there.</li>
           <li>The belief that we minister to each other as well as to those who call. We apply our training to everyday relationships, working together with respect for each person’s gifts.</li>
         </ol>
-        <p>The telephones of Contact are staffed 24 hours everyday of the year by trained volunteers. Every volunteer who works the lines at Contact receives 36 hours (24 class hours, 12 apprenticeship hours) of intense training to prepare them for the situations they will face on the lines.</p>
+
+        <p>
+          The telephones of Contact are staffed 24 hours everyday of the year by trained volunteers. Every volunteer who works the lines at Contact receives 36 hours (24 class hours, 12 apprenticeship hours)
+          of intense training to prepare them for the situations they will face on the lines.
+        </p>
+
         <p>People call Contact for a variety of reasons. We receive callers that are lonely, facing a crisis, and callers needing help with drug/alcohol addictions. We’re here to listen and help.</p>
         <p>More than 22,000 crisis calls are received each year. About 1.5% of those calls are suicide related calls. Of those calls, only about 1% are calls where the lethality is high. Professional support workers are always available to telephone workers when these types of calls are received. All calls are confidential. Telephone workers listen to callers with the highest regard for their value, dignity, and privacy.</p>
         <p>The mission statement of Contact Life Line of the Highland Rim, Inc. reads:</p>
         <p>It is the Christian ministry of Contact Life Line of the Highland Rim, Inc., as a non-profit organization with services free of charge to all callers, to respond 24 hours daily to the emotional needs of callers with confidential, non-judgmental, active listening by fostering self-directed resolution, offering referrals, or by intervention through telephone conversation with Contact volunteers.</p>
+        </section>
       </section>
     )
   }
